2cb46f01b5868bbca63a7e6fe45b270251817eb4
GonzaloNetka <gonzalonetka@gmail.com>
2023-08-08 22:30:22 -0300

    Second commit. Add Account, controllers and DTO. Task 2

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,11 +1,15 @@
 package com.mindhub.homebanking;
 
+import com.mindhub.homebanking.models.Account;
 import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.AccountRepository;
 import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class HomebankingApplication {
@@ -16,11 +20,25 @@ public class HomebankingApplication {
 
 
 	@Bean
-	public CommandLineRunner init(ClientRepository clientRepository){
+	public CommandLineRunner init(ClientRepository clientRepository ,AccountRepository accountRepository ){
 		return args -> {
-			Client client = new Client("Melba","Morel","melba@mindhub.com");
+			Client client1 = new Client("Melba","Morel","melba@mindhub.com");
+			Client client2 = new Client("Carlos","Machado","cmachado@mindhub.com");
 
-			clientRepository.save(client);
+			clientRepository.save(client1);
+			clientRepository.save(client2);
+
+			Account account1 = new Account("VIN001", LocalDate.now(),5000);
+			Account account2 = new Account("VIN002", LocalDate.now().plusDays(1),7500);
+			Account account3 = new Account("VIN003", LocalDate.now(),10000);
+
+			client1.addAccount(account1);
+			client1.addAccount(account2);
+			client2.addAccount(account3);
+
+			accountRepository.save(account1);
+			accountRepository.save(account2);
+			accountRepository.save(account3);
 
 
 		};
